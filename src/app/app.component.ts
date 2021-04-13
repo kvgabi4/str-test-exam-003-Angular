93b41c5aea470afa5066e84f6f456f25e0db392d
kvgabi4 <kvgabi4@gmail.com>
2021-04-13 14:47:47 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'str-test-exam003-Angular';
+}
