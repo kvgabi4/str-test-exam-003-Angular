a25789b87dff2b0a121d8d3e88d90d3ad746fb26
kvgabi4 <kvgabi4@gmail.com>
2021-04-13 15:13:16 +0200

    service

---
+++ src/app/service/customer.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Customer } from '../model/customer';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  apiUrl: string = 'https://nettuts.hu/jms/kvgabi4/customers';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(this.apiUrl);
+  };
+
+  get(id: number): Observable<Customer> {
+    return this.http.get<Customer>(`${this.apiUrl}/${id}`);
+  };
+
+  create(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(this.apiUrl, customer)
+  };
+
+  update(customer: Customer): Observable<Customer> {
+    return this.http.patch<Customer>(`${this.apiUrl}/${customer.id}`, customer);
+  };
+
+  delete(customer: Customer): Observable<Customer> {
+    return this.http.delete<Customer>(`${this.apiUrl}/${customer.id}`)
+  }
+}
